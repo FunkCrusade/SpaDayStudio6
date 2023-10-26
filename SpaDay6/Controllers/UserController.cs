4986867ab500a418352a4de319f41aaf557cfcaf
emilyalbert <FunkCrusade@outlook.com>
2023-10-25 19:24:04 -0500

    were really doing it

--- SpaDay6/Controllers/UserController.cs
+++ SpaDay6/Controllers/UserController.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SpaDay6.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SpaDay6.Controllers
 {
-    public class SpaController : Controller
+    public class UserController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
-        [HttpPost]
-        [Route("/spa")]
-        public IActionResult Menu(string skintype, string manipedi)
+
+        [HttpGet]
+        [Route("user/add")]
+        public IActionResult Add()
         {
-            Client newClient = new Client(skintype, manipedi);
-            newClient.SetFacials(newClient.SkinType);
-            ViewBag.client = newClient;
             return View();
         }
+
+
     }
 }
 
